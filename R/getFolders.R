8b4d67f83a3a393fbab717a665e3527278118290
Erik Palmore <erikpal@gmail.com>
2016-01-29 08:18:01 -0600

    Updated to work with processRequest revisions.

--- R/getFolders.R
+++ R/getFolders.R
@@ -1,48 +1,50 @@
 #' Get folders
 #' 
 #' Get a list of available reports
-#' @param uri The base uri of a Canvas installation
+#' @param url The base url of a Canvas installation
 #' @param ID The course, group, user or folder ID to return a list of folders of.
 #' @param IDtype The type of ID ("course", "group", "user", or "folder")
 #' @param folderID The ID of a folder if also providing a course or group ID.
 #' @param path Character of path to folder
-#' @param link Return vector of download location only.
 #' @param ... Optional page options to pass to processRequest
 #' @export
-getFolders <- function(uri, ID, IDtype = "course", folderID = "", path = "", ...) {
+getFolders <- function(url, ID, IDtype = "course", 
+                       folderID = NULL, path = NULL, ...) {
         
         ##Build the base url for the request
         ##Add in the api specific parameters
-        require(utils)
-        urlbase <- sub("uri", uri, "uri/api/v1/TYPE/ID/folders")
-       
+        require(httr)
+        url <- parse_url(url)
+        url$path <- "api/v1/TYPE/ID/folders"
+        
         if (IDtype == "course") {
-                urlbase <- sub("TYPE", "courses", urlbase)
+                url$path <- sub("TYPE", "courses", url$path)
         } else if (IDtype == "group") {
-                urlbase <- sub("TYPE", "groups", urlbase)
+                url$path <- sub("TYPE", "groups", url$path)
         } else if (IDtype == "folder") {
-                urlbase <- sub("TYPE", "folders", urlbase)
+                url$path <- sub("TYPE", "folders", url$path)
         } else if (IDtype == "user") {
-                urlbase <- sub("TYPE", "users", urlbase)
+                url$path <- sub("TYPE", "users", url$path)
         }
         
-        urlbase <- sub("ID", ID, urlbase)
+        url$path <- sub("ID", ID, url$path)
         
-        if (!folderID == "" & !path == "") {
+        if (!is.null(folderID) & !is.null(path)) {
                 stop("Please provide either folder ID or path, not both")
-        } else if (!folderID == "") {
-                urlbase <- paste0(urlbase, "/", folderID, "?")
-        } else if (!path == "") {
-                urlbase <- paste0(urlbase, "/by_path", path, "?")
+        } else if (!is.null(folderID)) {
+                url$path <- paste0(url$path, "/", folderID)
+                url$query <- list(exclude = NULL)
+        } else if (!is.null(path)) {
+                url$path <- paste0(url$path, "/by_path", path)
+                url$query <- list(exclude = NULL)
         } else {
-                urlbase <- paste0(urlbase, "?")
+                url$query <- list(exclude = NULL)
         }
         
-        urlbase <- URLencode(urlbase)
-        print(urlbase)
+        print(build_url(url))
         
         ##Pass the url to the request processor
-        results <- processRequest(urlbase, ...)
+        results <- processRequest(url, ...)
         
         return(results)
 }
--- R/getReports.R
+++ R/getReports.R
@@ -1,25 +1,27 @@
 #' Get list of available reports
 #' 
 #' Get a list of available reports
-#' @param uri The base uri of a Canvas installation
+#' @param url The base url of a Canvas installation
 #' @param accountID The account ID to retrieve reports from 
 #' @param titlenm The title of a specific report.  Leave blank for all reports.  Returns all if named report not avilable.
 #' @param link Return vector of download location only.
 #' @param ... Optional page options to pass to processRequest
 #' @export
-getReports <- function(uri, accountID, titlenm = "", link = FALSE, ...) {
+getReports <- function(url, accountID, titlenm = "", link = FALSE, ...) {
         
         ##Build the base url for the request
         ##Add in the api specific parameters
-        require(utils)
-        urlbase <- sub("uri", uri, "uri/api/v1/accounts/accountID/reports?")
-        urlbase <- sub("accountID", accountID, urlbase)
+        require(httr)
+        url <- parse_url(url)
+        url$path <- "api/v1/accounts/accountID/reports"
+        url$path <- sub("accountID", accountID, url$path)
 
-        urlbase <- URLencode(urlbase)
-        print(urlbase)
+        url$query <- list(exclude = NULL)
+        
+        print(build_url(url))
         
         ##Pass the url to the request processor
-        results <- processRequest(urlbase, ...)
+        results <- processRequest(url, ...)
         
         if (titlenm %in% results$title) {
                 results <- filter(results, title == titlenm)
