d00b1341d836d68967a0e5f382f291da4c1e637b
Erik Palmore <erikpal@gmail.com>
2016-01-28 22:12:07 -0600

    Working with processRequest revisions.

--- R/getAssignments.R
+++ R/getAssignments.R
@@ -4,38 +4,39 @@
 #' "enrollments","locked","avatar_url","bio") for this request have all been 
 #' enabled and can be subsetted out if not needed.  This excludes "test_student,"
 #' which has been set to its own argument.
-#' @param uri The base uri of a Canvas installation
+#' @param url The base url of a Canvas installation
 #' @param courseID Course ID to find assignments for
 #' @param search_term Filter results by partial course name, code, or full ID to match and return in the results list. Must be at least 3 characters.
 #' @param bucket Limit results to a named bucket ("past", "overdue", "undated", "ungraded", "upcoming", and "future")
 #' @param ... Optional page options to pass to processRequest
 #' @export
-getAssignments <- function(uri, courseID, search_term = "", bucket = NULL, ...) {
+getAssignments <- function(url, courseID, search_term = NULL, bucket = NULL, ...) {
         
         ##Build the base url for the request
         ##Add in the api specific parameters
-        require(utils)
-        urlbase <- sub("uri", uri, "uri/api/v1/courses/courseID/assignments?")
-        urlbase <- sub("courseID", courseID, urlbase)
-        urlbase <- paste0(urlbase, "&include[]=submission")
-        urlbase <- paste0(urlbase, "&include[]=assignment_visibility")
-        urlbase <- paste0(urlbase, "&include[]=all_dates")
-        urlbase <- paste0(urlbase, "&include[]=overrides")
-        urlbase <- paste0(urlbase, "&include[]=observed_users")
+        require(httr)
+        url <- parse_url(url)
+        url$path <- "api/v1/courses/courseID/assignments"
+        url$path <- sub("courseID", courseID, url$path)
         
-        if (nchar(search_term) > 2) {
-                urlbase <- paste0(urlbase, "&search_term=", search_term)
-        }
-
-        if (!is.null(bucket)) {
-                urlbase <- paste0(urlbase, "&bucket=", bucket)
+        url$query <- list("include[]" = "submission",
+                          "include[]" = "assignment_visibility",
+                          "include[]" = "all_dates",
+                          "include[]" = "overrides",
+                          "include[]" = "observed_users",
+                          search_term = search_term,
+                          bucket = bucket)
+        
+        if (!is.null(search_term)) {
+                if (nchar(search_term) < 2) {
+                        warning("Search term must be three or more characters.")
+                }
         }
         
-        urlbase <- URLencode(urlbase)
-        print(urlbase)
+        print(build_url(url))
         
         ##Pass the url to the request processor
-        results <- processRequest(urlbase, ...)
+        results <- processRequest(url, ...)
         
         return(results)
 }
--- R/getUser.R
+++ R/getUser.R
@@ -1,24 +1,26 @@
 #' Get details of a specific user
 #' 
 #' Get user details for the provided user id. 
-#' @param uri The base uri of a Canvas installation
-#' @param userID Course ID to find assignments for
+#' @param url The base url of a Canvas installation
+#' @param userID User ID to find assignments for
 #' @param ... Optional page options to pass to processRequest
 #' @export
 ##TODO: This technically works but returns an error in processRequest
-getUser <- function(uri, userID, ...) {
+getUser <- function(url, userID, ...) {
         
         ##Build the base url for the request
         ##Add in the api specific parameters
-        require(utils)
-        urlbase <- sub("uri", uri, "uri/api/v1/users/userID?")
-        urlbase <- sub("userID", userID, urlbase)
+        require(httr)
+        url <- parse_url(url)
+        url$path <- "api/v1/users/userID"
+        url$path <- sub("userID", userID, url$path)
 
-        urlbase <- URLencode(urlbase)
-        print(urlbase)
+        url$query <- list(exclude = NULL)
+        
+        print(build_url(url))
         
         ##Pass the url to the request processor
-        results <- processRequest(urlbase, ...)
+        results <- processRequest(url, ...)
         
         return(results)
 }
