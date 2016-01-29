27ce4362da05657f673fb4c1384180a3b7b9dec8
Erik Palmore <erikpal@gmail.com>
2016-01-28 21:20:03 -0600

    Works with processRequest revisions.

--- R/getCourseUsers.R
+++ R/getCourseUsers.R
@@ -4,7 +4,7 @@
 #' "enrollments","locked","avatar_url","bio") for this request have all been 
 #' enabled and can be subsetted out if not needed.  This excludes "test_student,"
 #' which has been set to its own argument.
-#' @param uri The base uri of a Canvas installation
+#' @param url The base url of a Canvas installation
 #' @param courseID Course ID to find users for
 #' @param search_term Filter results by partial course name, code, or full ID to match and return in the results list. Must be at least 3 characters.
 #' @param user_ids Vector of user IDs to limit results by
@@ -14,51 +14,47 @@
 #' @param state Filter results to include only users with one of specified string ("active","invited","rejected","completed","inactive")
 #' @param ... Optional page options to pass to processRequest
 #' @export
-getCourseUsers <- function(uri, courseID, search_term = "", user_ids = NULL,
+getCourseUsers <- function(url, courseID, search_term = NULL, user_ids = NULL,
                            test_student = FALSE, type = NULL, role_id = NULL, 
                            state = NULL, ...) {
         
         ##Build the base url for the request
         ##Add in the api specific parameters
-        require(utils)
-        urlbase <- sub("uri", uri, "uri/api/v1/courses/courseID/users?")
-        urlbase <- sub("courseID", courseID, urlbase)
-        urlbase <- paste0(urlbase, "&include[]=email")
-        urlbase <- paste0(urlbase, "&include[]=enrollments")
-        urlbase <- paste0(urlbase, "&include[]=locked")
-        urlbase <- paste0(urlbase, "&include[]=avatar_url")
-        urlbase <- paste0(urlbase, "&include[]=bio")
+        require(httr)
+        url <- parse_url(url)
+        url$path <- "api/v1/courses/courseID/users"
+        url$path <- sub("courseID", courseID, url$path)
         
-        if (nchar(search_term) > 2) {
-                urlbase <- paste0(urlbase, "&search_term=", search_term)
-        }
+        url$query <- list("include[]" = "email",
+                          "include[]" = "enrollments",
+                          "include[]" = "locked",
+                          "include[]" = "avatar_url",
+                          "include[]" = "bio",
+                          search_term = search_term,
+                          "enrollment_type[]" = type,
+                          "enrollment_type[]" = role_id,
+                          "enrollment_state[]" = state)
         
         if (test_student == TRUE) {
-                urlbase <- paste0(urlbase, "&include[]=test_student")
+                url$query <- c(url$query, "include[]" = "test_student")
         }
         
-        if (!is.null(type)) {
-                urlbase <- paste0(urlbase, "&enrollment_type[]=", type)
+        user_ids_list <- NULL
+        for (i in user_ids) {
+                user_ids_list <- c(user_ids_list, "user_ids[]" = i)
+        }
+        url$query <- c(url$query, user_ids_list)
+        
+        if (!is.null(search_term)) {
+                if (nchar(search_term) < 2) {
+                        warning("Search term must be three or more characters.")
+                }
         }
         
-        if (!is.null(role_id)) {
-                urlbase <- paste0(urlbase, "&enrollment_type[]=", role_id)
-        }
-        
-        if (!is.null(user_ids)) {
-                paramUserIDS <- parameterVString("user_ids", user_ids, ...)
-                urlbase <- paste0(urlbase, paramUserIDS)
-        }
-        
-        if (!is.null(state)) {
-                urlbase <- paste0(urlbase, "&enrollment_state[]=", state)
-        }
-        
-        urlbase <- URLencode(urlbase)
-        print(urlbase)
+        print(build_url(url))
         
         ##Pass the url to the request processor
-        results <- processRequest(urlbase, ...)
+        results <- processRequest(url, ...)
         
         return(results)
 }
