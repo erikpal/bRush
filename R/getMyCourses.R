28a98cf21492ebd43dceb84129f2de90d41f8787
Erik Palmore <erikpal@gmail.com>
2016-01-28 21:40:59 -0600

    Working with processRequest revisions.

--- R/getMyCourses.R
+++ R/getMyCourses.R
@@ -3,49 +3,40 @@
 #' Get course details for the account requesting. All include parameters (see API
 #' documentation) for this request have all been enabled and can be subsetted out 
 #' if not needed.
-#' @param uri The base uri of a Canvas installation
+#' @param url The base url of a Canvas installation
 #' @param type Filter results to include course with at least one of specified string ("teacher","student","ta","observer","designer")
 #' @param role_id Integer of role ID to limit results.
 #' @param state String of enrollment state to limit results to (“unpublished”,“available”,“completed”,“deleted”)
 #' @param ... Optional page options to pass to processRequest
 #' @export
-getMyCourses <- function(uri, type = NULL, role_id = NULL, 
-                         state = NULL, ...) {
+getMyCourses <- function(url, type = NULL, role_id = NULL, state = NULL, ...) {
         
         ##Build the base url for the request
         ##Add in the api specific parameters
-        require(utils)
-        urlbase <- sub("uri", uri, "uri/api/v1/courses?")
-        urlbase <- paste0(urlbase, "&include[]=needs_grading_count")
-        urlbase <- paste0(urlbase, "&include[]=syllabus_body")
-        urlbase <- paste0(urlbase, "&include[]=term")
-        urlbase <- paste0(urlbase, "&include[]=course_progress")
-        urlbase <- paste0(urlbase, "&include[]=sections")
-        urlbase <- paste0(urlbase, "&include[]=storage_quota_used_mb")
-        urlbase <- paste0(urlbase, "&include[]=total_students")
-        urlbase <- paste0(urlbase, "&include[]=passback_status")
-        urlbase <- paste0(urlbase, "&include[]=favorites")
-        urlbase <- paste0(urlbase, "&include[]=teachers")
-        urlbase <- paste0(urlbase, "&include[]=observed_users")
-        urlbase <- paste0(urlbase, "&include[]=total_scores")
+        require(httr)
+        url <- parse_url(url)
+        url$path <- "api/v1/courses"
+
+        url$query <- list("include[]" = "needs_grading_count",
+                          "include[]" = "syllabus_body",
+                          "include[]" = "term",
+                          "include[]" = "course_progress",
+                          "include[]" = "sections",
+                          "include[]" = "storage_quota_used_mb",
+                          "include[]" = "total_students",
+                          "include[]" = "passback_status",
+                          "include[]" = "favorites",
+                          "include[]" = "teachers",
+                          "include[]" = "observed_users",
+                          "include[]" = "total_scores",
+                          enrollment_type = type,
+                          enrollment_role_id = role_id,
+                          "state[]" = state)
         
-        if (!is.null(type)) {
-                urlbase <- paste0(urlbase, "&enrollment_type=", type)
-        }
-        
-        if (!is.null(role_id)) {
-                urlbase <- paste0(urlbase, "&enrollment_role_id=", role_id)
-        }
-        
-        if (!is.null(state)) {
-                urlbase <- paste0(urlbase, "&state[]=", state)
-        }
-        
-        urlbase <- URLencode(urlbase)
-        print(urlbase)
+        print(build_url(url))
         
         ##Pass the url to the request processor
-        results <- processRequest(urlbase, ...)
+        results <- processRequest(url, ...)
         
         return(results)
 }
--- R/getUserCourses.R
+++ R/getUserCourses.R
@@ -3,38 +3,35 @@
 #' Get course details for the account requesting. All include parameters (see API
 #' documentation) for this request have all been enabled and can be subsetted out 
 #' if not needed.
-#' @param uri The base uri of a Canvas installation
+#' @param url The base url of a Canvas installation
 #' @param userID The string of a user to request courses of
 #' @param state String of enrollment state to limit results to (“unpublished”,“available”,“completed”,“deleted”)
 #' @param ... Optional page options to pass to processRequest
 #' @export
-getUserCourses <- function(uri, userID, type = NULL, enrollment_role_id = NULL, 
-                         state = NULL, ...) {
+getUserCourses <- function(url, userID, state = NULL, ...) {
         
         ##Build the base url for the request
         ##Add in the api specific parameters
-        require(utils)
-        urlbase <- sub("uri", uri, "uri/api/v1/users/userID/courses?")
-        urlbase <- sub("userID", userID, urlbase)
-        urlbase <- paste0(urlbase, "&include[]=needs_grading_count")
-        urlbase <- paste0(urlbase, "&include[]=syllabus_body")
-        urlbase <- paste0(urlbase, "&include[]=term")
-        urlbase <- paste0(urlbase, "&include[]=course_progress")
-        urlbase <- paste0(urlbase, "&include[]=sections")
-        urlbase <- paste0(urlbase, "&include[]=storage_quota_used_mb")
-        urlbase <- paste0(urlbase, "&include[]=total_students")
-        urlbase <- paste0(urlbase, "&include[]=favorites")
-        urlbase <- paste0(urlbase, "&include[]=total_scores")
+        require(httr)
+        url <- parse_url(url)
+        url$path <- "api/v1/users/userID/courses"
+        url$path <- sub("userID", userID, url$path)
         
-        if (!is.null(state)) {
-                urlbase <- paste0(urlbase, "&state[]=", state)
-        }
+        url$query <- list("include[]" = "needs_grading_count",
+                          "include[]" = "syllabus_body",
+                          "include[]" = "term",
+                          "include[]" = "course_progress",
+                          "include[]" = "sections",
+                          "include[]" = "storage_quota_used_mb",
+                          "include[]" = "total_students",
+                          "include[]" = "favorites",
+                          "include[]" = "total_scores",
+                          "state[]" = state)
         
-        urlbase <- URLencode(urlbase)
-        print(urlbase)
+        print(build_url(url))
         
         ##Pass the url to the request processor
-        results <- processRequest(urlbase, ...)
+        results <- processRequest(url, ...)
         
         return(results)
 }
