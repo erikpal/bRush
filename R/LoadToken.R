270ba1cf5a480d3f6655817fb321486d864a9059
Erik Palmore <erikpal@gmail.com>
2017-04-20 17:24:42 -0500

    Now works with .Renviron key storage.

--- R/LoadToken.R
+++ R/LoadToken.R
@@ -1,15 +1,23 @@
 #' Load API Token
 #' 
-#' Looks in the working directory for txt file with token.
+#' Assumes that the Canvas API key is in the .Renviron file.  Default name of variable is "CanvasApiKey."
+#' If absent, looks in the working directory for txt file with token named token.txt.
+#' @param env_var_name Character of the name of the env variable 
 #' @return token A character vector of token
 
-loadToken <- function() {
-        if ("token.txt" %in% list.files(".")) {
-                con <- file("token.txt", "r")
-                token <- readLines(con, n = 1, warn = FALSE)
-                close(con)
+loadToken <- function(env_var_name) {
+        
+        if (Sys.getenv(env_var_name) == "") {
+                if ("token.txt" %in% list.files(".")) {
+                        con <- file("token.txt", "r")
+                        token <- readLines(con, n = 1, warn = FALSE)
+                        close(con)
+                } else {
+                        stop(cat("Please place token file in .Renviron or in working directory as 'token.txt'."))
+                }
         } else {
-                stop("Please place token file in working directory as 'token.txt'.")
+                token <- Sys.getenv(env_var_name) 
         }
+        
         return(token)
 }
